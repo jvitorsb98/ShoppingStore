4722e2d088ff4cd227130f1a2eafe18752442f4c
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-05-14 23:24:13 -0300

    Creating possibileFacets

---
+++ ShoppingStore/ShoppingStore/src/test/java/br/com/cepedi/ShoppingStore/ShoppingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cepedi.ShoppingStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
