15a5fb6307b8a5d7734fc2cb375a05bc1a890fe4
IpvLeo <leoanarcopunk77@gmail.com>
2024-05-16 23:06:15 -0300

    testando entity do audit

--- ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/audit/entitys/AuditLog.java
+++ ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/audit/entitys/AuditLog.java
@@ -10,9 +10,12 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
