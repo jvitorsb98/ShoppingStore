da070508c594bb1da226ddea64d164d4603c5c0f
IpvLeo <leoanarcopunk77@gmail.com>
2024-05-18 00:56:39 -0300

    Criando o service Category

--- ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/model/records/category/details/DataCategoryDetails.java
+++ ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/model/records/category/details/DataCategoryDetails.java
@@ -1,7 +1,8 @@
 package br.com.cepedi.ShoppingStore.model.records.category.details;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
+@AllArgsConstructor
 @Data
 public class DataCategoryDetails {
     private Long id;
