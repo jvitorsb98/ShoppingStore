9e2af6cbadd5b704bfd88685bde7bf505765a4d0
danilo775 <danilo.uba@hotmail.com>
2024-05-20 09:15:42 -0300

    metodo buscar em productRatingService

--- ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/repository/ProductRatingRepository.java
+++ ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/repository/ProductRatingRepository.java
@@ -1,7 +1,20 @@
 package br.com.cepedi.ShoppingStore.repository;
 
 import br.com.cepedi.ShoppingStore.model.entitys.ProductRating;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRatingRepository extends JpaRepository<ProductRating,Long> {
+	
+	Page<ProductRating> findAll(Pageable pageable);
+	
+	@Query("""
+            SELECT p.ratingStars, p.review, p.user, p.product from ProductRating p
+                WHERE p.id = :id
+            """)
+	Boolean findActivatedById(Long id);
+	
 }
--- ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/service/productRating/ProductRatingService.java
+++ ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/service/productRating/ProductRatingService.java
@@ -3,10 +3,13 @@ package br.com.cepedi.ShoppingStore.service.productRating;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.cepedi.ShoppingStore.model.entitys.Product;
 import br.com.cepedi.ShoppingStore.model.entitys.ProductRating;
+import br.com.cepedi.ShoppingStore.model.records.product.details.DataProductDetails;
 import br.com.cepedi.ShoppingStore.model.records.productAttribute.details.DataProductAttributeDetails;
 import br.com.cepedi.ShoppingStore.model.records.productRating.input.DataRegisterProductRating;
 import br.com.cepedi.ShoppingStore.model.records.productRating.input.details.DataProductRatingDetails;
@@ -40,5 +43,12 @@ public class ProductRatingService {
 		return new DataProductRatingDetails(productRating);
 	}
 	
-
+	public Page<DataProductRatingDetails> list(Pageable pageable) {
+        return productRatingRepository.findAll(pageable).map(DataProductRatingDetails::new);
+    }
+	
+	public DataProductRatingDetails detailsProduct(Long id) {
+		return new DataProductRatingDetails(productRatingRepository.getReferenceById(id));
+	}
+	
 }
