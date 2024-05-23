a25f41f497fd76562bc4e78d8eb634487419fdb7
danilo775 <danilo.uba@hotmail.com>
2024-05-23 09:35:29 -0300

    create busca

--- ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/controller/productAttribute/ProductAttributeController.java
+++ ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/controller/productAttribute/ProductAttributeController.java
@@ -18,9 +18,11 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.cepedi.ShoppingStore.model.records.productAttribute.details.DataProductAttributeDetails;
 import br.com.cepedi.ShoppingStore.model.records.productAttribute.input.DataRegisterProductAttribute;
+import br.com.cepedi.ShoppingStore.model.records.productAttribute.input.DataUpdateProductAttribute;
 import br.com.cepedi.ShoppingStore.service.productAttribute.ProductAttributeService;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v2/productsAttributte")
@@ -39,7 +41,7 @@ public class ProductAttributeController {
         log.info("Scheduling new ProductAttribute...");
         DataProductAttributeDetails details = service.register(data);
         URI uri = uriBuilder.path("/product-Attribute/{id}").buildAndExpand(details.id()).toUri();
-        log.info("ProductAttribute scheduled successfully.");
+        log.info("ProductAttribute registered successfully with ID: {}", details.id());
         return ResponseEntity.created(uri).body(details);
 	}
     
@@ -47,7 +49,7 @@ public class ProductAttributeController {
     public ResponseEntity<Page<DataProductAttributeDetails>> list(@PageableDefault(size = 5, sort = {"id"}) Pageable pageable) {
         log.info("Fetching list of ProductAttribute...");
         Page<DataProductAttributeDetails> page = service.list(pageable);
-        log.info("List of ProductAttribute fetched successfully.");
+        log.info("List of deactivated ProductAttributes fetched successfully. Total elements: {}", page.getTotalElements());
         return ResponseEntity.ok(page);
     }
     
@@ -55,7 +57,33 @@ public class ProductAttributeController {
     public ResponseEntity<Page<DataProductAttributeDetails>> listDeactivated(@PageableDefault(size = 5, sort = {"id"}) Pageable pageable) {
         log.info("Fetching list of ProductAttribute...");
         Page<DataProductAttributeDetails> page = service.listDeactivated(pageable);
-        log.info("List of ProductAttribute fetched successfully.");
+        log.info("List of ProductAttributes fetched successfully. Total elements: {}", page.getTotalElements());
         return ResponseEntity.ok(page);
     }
+    
+    @GetMapping("/product/{id}")
+    public ResponseEntity<List<DataProductAttributeDetails>> listByProductId(@PathVariable Long id) {
+    	log.info("Fetching list of ProductAttributes for product with ID: {}", id);
+        List<DataProductAttributeDetails> attributes = service.listByProductId(id);
+        log.info("List of ProductAttributes for product with ID: {} fetched successfully. Total elements: {}", id, attributes.size());
+        return ResponseEntity.ok(attributes);
+    }
+    
+    @GetMapping("/productDactivated/{id}")
+    public ResponseEntity<List<DataProductAttributeDetails>> listByProductIdDeactivated(@PathVariable Long id) {
+		log.info("Fetching list of ProductAttributes for product with ID: {}", id);
+        List<DataProductAttributeDetails> attributes = service.listByProductIdDeactivated(id);
+		log.info("List of ProductAttributes for product with ID: {} fetched successfully. Total elements: {}", id, attributes.size());
+        return ResponseEntity.ok(attributes);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<DataProductAttributeDetails> getDetails(@PathVariable Long id) {
+		log.info("Fetching details of ProductAttribute with ID: {}", id);
+        DataProductAttributeDetails details = service.details(id);
+		log.info("Details of ProductAttribute with ID {} fetched successfully.", id);
+        return ResponseEntity.ok(details);
+    }
+    
+
 }
