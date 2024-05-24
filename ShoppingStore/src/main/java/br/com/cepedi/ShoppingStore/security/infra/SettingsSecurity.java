f065209382fab8e5355cbfe375df20af04686ebd
João Ramos <jvitorsb98@outlook.com>
2024-05-24 14:34:28 -0300

    aplicando permissoes

--- ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/security/infra/SettingsSecurity.java
+++ ShoppingStore/src/main/java/br/com/cepedi/ShoppingStore/security/infra/SettingsSecurity.java
@@ -3,6 +3,7 @@ package br.com.cepedi.ShoppingStore.security.infra;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,7 +31,39 @@ public class SettingsSecurity {
                     req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     req.requestMatchers("/auth/reset-password/**").permitAll();
                     req.requestMatchers("/auth/activate-account").permitAll();
-                    req.anyRequest().permitAll();
+
+                    req.requestMatchers(HttpMethod.POST, "/api/v2/brands/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.PUT, "/api/v2/brands/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.DELETE, "/api/v2/brands/**").hasRole("ADMIN");
+
+                    req.requestMatchers(HttpMethod.POST, "/api/v2/categories/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.PUT, "/api/v2/categories/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.DELETE, "/api/v2/categories/**").hasRole("ADMIN");
+
+                    req.requestMatchers(HttpMethod.POST, "/api/v2/payments/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.PUT, "/api/v2/payments/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.DELETE, "/api/v2/payments/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.GET, "/api/v2/payments/**").hasRole("ADMIN");
+
+                    req.requestMatchers(HttpMethod.POST, "/api/v2/possible-facets/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.PUT, "/api/v2/possible-facets/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.DELETE, "/api/v2/possible-facets/**").hasRole("ADMIN");
+
+                    req.requestMatchers(HttpMethod.POST, "/api/v2/products/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.PUT, "/api/v2/products/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.DELETE, "/api/v2/products/**").hasRole("ADMIN");
+
+                    req.requestMatchers(HttpMethod.POST, "/api/v2/productsAttributte/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.PUT, "/api/v2/productsAttributte/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.DELETE, "/api/v2/productsAttributte/**").hasRole("ADMIN");
+
+                    req.requestMatchers(HttpMethod.POST, "/api/v2/productsRating/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.PUT, "/api/v2/productsRating/**").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.DELETE, "/api/v2/productsRating/**").hasRole("ADMIN");
+
+
+                    req.anyRequest().authenticated();
+
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
--- ShoppingStore/src/main/resources/db/migration/V4__create-table-tokens.sql
+++ ShoppingStore/src/main/resources/db/migration/V4__create-table-tokens.sql
@@ -2,7 +2,7 @@ CREATE TABLE tokens (
     id BIGSERIAL PRIMARY KEY,
     token VARCHAR(255) NOT NULL,
     user_id BIGINT,
-    expire_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    expire_date TIMESTAMP WITH TIME ZONE,
     disabled BOOLEAN,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
