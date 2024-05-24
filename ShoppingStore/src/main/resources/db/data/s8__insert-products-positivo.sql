da0e8b6d15ded909fa5274af4b64d91fdeec18e4
João Ramos <jvitorsb98@outlook.com>
2024-05-24 03:45:29 -0300

    metade servers positivo

---
+++ ShoppingStore/src/main/resources/db/data/s8__insert-products-positivo.sql
@@ -0,0 +1,228 @@
+-- Inserindo informações do servidor Positivo Rack SYS-611C-TN4R (SYS-611C-TN4R) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-611C-TN4R',
+        'Servidor Rack Dual SYS-611C-TN4R 1U',
+        1000.00,
+        'SYS-611C-TN4R',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-611C-TN4R-1.jpg',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-121H-TNR (SYS-121H-TNR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-121H-TNR',
+        'Servidor Rack Dual SYS-121H-TNR1U',
+        1000.00,
+        'SYS-121H-TNR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-121H-TNR.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack AS-1125HS-TNR (AS-1125HS-TNR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack AS-1125HS-TNR',
+        'Servidor Rack Dual AS-1125HS-TNR',
+        1000.00,
+        'AS-1125HS-TNR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/AS-1125HS-TNR.png',
+        1,
+        100,
+        2,
+        true);
+
+
+-- Inserindo informações do servidor Positivo Rack SYS-620P-TR-BR (SYS-620P-TR-BR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-620P-TR-BR',
+        'Servidor Rack Dual SYS-620P-TR-BR',
+        1000.00,
+        'SYS-620P-TR-BR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2018/08/SYS-620P-TR.jpg',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack AS-2024S-TR (AS-2024S-TR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack AS-2024S-TR',
+        'Servidor Rack Dual AS-2024S-TR',
+        1000.00,
+        'AS-2024S-TR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2018/08/AS-2024S-TR.jpg',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-621P-TR (SYS-621P-TR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-621P-TR',
+        'Servidor Rack Dual SYS-621P-TR',
+        1000.00,
+        'SYS-621P-TR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-621P-TR.jpg',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-621C-TN12R (SYS-621C-TN12R) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-621C-TN12R',
+        'Servidor Rack Dual SYS-621C-TN12R',
+        1000.00,
+        'SYS-621C-TN12R',
+        'https://www.positivoservers.com.br/wp-content/uploads/2018/08/SYS-621C-TN12R.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-621H-TN12R (SYS-621H-TN12R) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-621H-TN12R',
+        'Servidor Rack Dual SYS-621H-TN12R',
+        1000.00,
+        'SYS-621H-TN12R',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-621H-TN12R.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-511E-WR (SYS-511E-WR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-511E-WR',
+        'Servidor Rack Dual SYS-511E-WR',
+        1000.00,
+        'SYS-511E-WR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-511E-WR.jpg',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack AS -1115HS-TNR (AS -1115HS-TNR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack AS -1115HS-TNR',
+        'Servidor Rack Dual AS -1115HS-TNR',
+        1000.00,
+        'AS -1115HS-TNR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/AS-1115HS-TNR.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-521E-WR(SYS-521E-WR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-521E-WR',
+        'Servidor Rack Dual SYS-521E-WR',
+        1000.00,
+        'SYS-521E-WR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-521E-WR_main.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack AS -2015HS-TNR(AS -2015HS-TNR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack AS -2015HS-TNR',
+        'Servidor Rack Dual AS -2015HS-TNR',
+        1000.00,
+        'AS -2015HS-TNR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/AS-2015HS-TNR.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-211TP-HPTR(SYS-211TP-HPTR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-211TP-HPTR',
+        'Servidor Rack Dual SYS-211TP-HPTR',
+        1000.00,
+        'SYS-211TP-HPTR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-211TP-HPTR.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack AS-2115GT-HNTF(AS-2115GT-HNTF) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack AS-2115GT-HNTF',
+        'Servidor Rack Dual AS-2115GT-HNTF',
+        1000.00,
+        'AS-2115GT-HNTF',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/AS-2115GT-HNTF.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-621BT-HNT(SYS-621BT-HNT) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-621BT-HNT',
+        'Servidor Rack Dual SYS-621BT-HNT',
+        1000.00,
+        'SYS-621BT-HNT',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-621BT-HNTR.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-221BT-HNR(SYS-221BT-HNR) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-221BT-HNR',
+        'Servidor Rack Dual SYS-221BT-HNR',
+        1000.00,
+        'SYS-221BT-HNR',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-221BT-HNR.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo Rack SYS-F511E2-RT(SYS-F511E2-RT) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor Rack SYS-F511E2-RT',
+        'Servidor Rack Dual SYS-F511E2-RT',
+        1000.00,
+        'SYS-F511E2-RT',
+        'https://www.positivoservers.com.br/wp-content/uploads/2023/12/SYS-F511E2-RT.png',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo SuperServer 6029P-E1CR12H(SuperServer 6029P-E1CR12H) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor SuperServer 6029P-E1CR12H',
+        'Servidor SuperServer 6029P-E1CR12H',
+        1000.00,
+        '6029P-E1CR12H',
+        'https://www.positivoservers.com.br/wp-content/uploads/2018/08/SuperServer-6029P-E1CR12H.jpg',
+        1,
+        100,
+        2,
+        true);
+
+-- Inserindo informações do servidor Positivo SuperServer 6039P-E1CR16H(SuperServer 6039P-E1CR16H) na tabela "product"
+INSERT INTO product (name, description, price, sku, image_url, category_id, quantity, brand_id, featured)
+VALUES ('Servidor SuperServer 6039P-E1CR16H',
+        'Servidor SuperServer 6039P-E1CR16H',
+        1000.00,
+        '6039P-E1CR16H',
+        'https://www.positivoservers.com.br/wp-content/uploads/2018/08/SuperServer-6039P-E1CR16H.jpg',
+        1,
+        100,
+        2,
+        true);
---
+++ ShoppingStore/src/main/resources/db/data/s9__insert-product-attribute-products-positivo.sql
@@ -0,0 +1,180 @@
+-- Inserindo um atributtes do servidor Positivo PowerEdgeSYS-611C-TN4R (SYS-611C-TN4R) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (30, 'Modelo', 'Rack SYS-611C-TN4R'),
+       (30, 'Tipo', 'Servidor Rack 1U'),
+       (30, 'Processador', '2x Xeon® Scalable de 4th geração'),
+       (30, 'Memória', 'Suporta até 4TB, DDR5 4800MHz ECC RDIMM, 16 slots DIMM'),
+       (30, 'Armazenamento', '4x 3.5″ hot-swap NVMe/SATA/SAS'),
+       (30, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-121H-TNR (SYS-121H-TNR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (31, 'Modelo', 'Rack SYS-121H-TNR'),
+       (31, 'Tipo', 'Servidor Rack 1U'),
+       (31, 'Processador', '2x Xeon® Scalable de 4th geração'),
+       (31, 'Memória', 'Suporta até 8TB, 4800MHz ECC DDR5 32 slots DIMM'),
+       (31, 'Armazenamento', '8x 2.5″ hot-swap NVMe/SATA/SAS'),
+       (31, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge AS-1125HS-TNR (AS-1125HS-TNR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (32, 'Modelo', 'Rack AS-1125HS-TNR'),
+       (32, 'Tipo', 'Servidor Rack 1U'),
+       (32, 'Processador', '2x AMD EPYC™ 9004 Series Processors'),
+       (32, 'Memória', 'Suporta até 6TB, 4800MHz ECC DDR5 RDIMM 24 slots DIMM'),
+       (32, 'Armazenamento', '8x 2.5″ hot-swap NVMe/SATA/SAS'),
+       (32, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge AS-1125HS-TNR (AS-1125HS-TNR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (33, 'Modelo', 'Rack AS-1125HS-TNR'),
+       (33, 'Tipo', 'Servidor Rack 2U'),
+       (33, 'Processador', '2x Xeon® Scalable de 3th geração'),
+       (33, 'Memória', 'Suporta até 4TB, 3200MHz ECC LRDIMM/RDIMM DDR4 16 slots DIMM'),
+       (33, 'Armazenamento', 'Não informado'),
+       (33, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge AS-2024S-TR(AS-2024S-TR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (34, 'Modelo', 'Rack AS-2024S-TR'),
+       (34, 'Tipo', 'Servidor Rack 2U'),
+       (34, 'Processador', '2x AMD EPYC™ 7002/7003 Series'),
+       (34, 'Memória', 'Suporta até 4TB, DDR4-3200MHz ECC RDIMM, LRDIMM 16 slots DIMM'),
+       (34, 'Armazenamento', '12x 3.5″ hot-swap SATA/NVME'),
+       (34, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-621P-TR(SYS-621P-TR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (35, 'Modelo', 'Rack SYS-621P-TR'),
+       (35, 'Tipo', 'Servidor Rack 2U'),
+       (35, 'Processador', '2x Xeon® Scalable de 4a geração'),
+       (35, 'Memória', 'Suporta até 4TB 4800MHz ECC DDR5 RDIMM 16 slots DIMM'),
+       (35, 'Armazenamento', '8x 3.5″ hot-swap SATA3'),
+       (35, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-621C-TN12R (SYS-621C-TN12R) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (36, 'Modelo', 'SYS-621C-TN12R'),
+       (36, 'Tipo', 'Servidor Rack 2U'),
+       (36, 'Processador', '2x Xeon® Scalable de 4a geração'),
+       (36, 'Memória', 'Suporta até 1TB ECC 3DS LRDIMM DDR4-2666MHz; 8 slots DIMM'),
+       (36, 'Armazenamento', '12x 3.5″ hot-swap NVMe/SATA/SAS'),
+       (36, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-621C-TN12R (SYS-621C-TN12R) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (37, 'Modelo', 'SYS-621C-TN12R'),
+       (37, 'Tipo', 'Servidor Rack 2U'),
+       (37, 'Processador', '2x Xeon® Scalable de 4a geração'),
+       (37, 'Memória', 'Suporta até 8TB, RDIMM, DDR5-4800MHz ECC 32 slots DIMM'),
+       (37, 'Armazenamento', '12x 3.5″ hot-swap NVMe/SATA/SAS'),
+       (37, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-511E-WR(SYS-511E-WR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (38, 'Modelo', 'SYS-511E-WR'),
+       (38, 'Tipo', 'Servidor Rack monoprocessado 1U'),
+       (38, 'Processador', '2x Xeon® Scalable de 4a geração'),
+       (38, 'Memória', 'Suporta até 2TB 4800/4400MHz ECC DDR5 RDIMM 8 slots DIMM'),
+       (38, 'Armazenamento', '4x 3.5″ SATA/SAS'),
+       (38, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge AS -1115HS-TNR(AS -1115HS-TNR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (39, 'Modelo', 'AS -1115HS-TNR'),
+       (39, 'Tipo', 'Servidor Rack monoprocessado 1U'),
+       (39, 'Processador', 'AMD EPYC™ 9004'),
+       (39, 'Memória', 'Suporta até 6TB 4000/3600 MHz ECC DDR5 RDIMM 24 slots DIMM'),
+       (39, 'Armazenamento', '8x 2.5″ hot-swap NVMe/SATA/SAS'),
+       (39, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge AS -1115HS-TNR(AS -1115HS-TNR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (40, 'Modelo', 'AS -1115HS-TNR'),
+       (40, 'Tipo', 'Servidor Rack monoprocessado 2U'),
+       (40, 'Processador', 'Xeon® Scalable de 4a geração'),
+       (40, 'Memória', 'Suporta até 2TB 4800/4400MHz ECC DDR5 RDIMM 8 slots DIMM'),
+       (40, 'Armazenamento', '8x 3.5″ NVMe/SATA/SAS'),
+       (40, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge AS -2015HS-TNR(AS -2015HS-TNR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (41, 'Modelo', 'AS -2015HS-TNR'),
+       (41, 'Tipo', 'Servidor Rack monoprocessado 2U'),
+       (41, 'Processador', 'AMD EPYC™ 9004 Series'),
+       (41, 'Memória', 'Suporta até 6TB 4000/3600 MHz ECC DDR5 RDIMM 24 slots DIMM'),
+       (41, 'Armazenamento', '2x 3.5″ hot-swap NVMe/SATA/SAS'),
+       (41, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge AS -2015HS-TNR(AS -2015HS-TNR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (42, 'Modelo', 'AS -2015HS-TNR'),
+       (42, 'Tipo', 'RACK MULTINODE TWIN 2U'),
+       (42, 'Processador', '2x Xeon® Scalable de 4th geração'),
+       (42, 'Memória', 'Suporta até 2TB, 4800MHz ECC DDR5 R-DIMM 8 slots DIMM'),
+       (42, 'Armazenamento', '6x 2.5″ hot-swap SATA'),
+       (42, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge AS-2115GT-HNTF(AS-2115GT-HNTF) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (43, 'Modelo', 'AS-2115GT-HNTF'),
+       (43, 'Tipo', 'RACK MULTINODE TWIN 2U'),
+       (43, 'Processador', '2x AMD EPYC™ 9004 Series'),
+       (43, 'Memória', 'Suporta até 3TB, 4800MHz ECC DDR5 RDIMM 12 slots DIMM'),
+       (43, 'Armazenamento', '4x 2.5″ hot-swap NVMe/SATA'),
+       (43, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-621BT-HNT(SYS-621BT-HNT) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (44, 'Modelo', 'SYS-621BT-HNT'),
+       (44, 'Tipo', 'RACK MULTINODE TWIN 2U'),
+       (44, 'Processador', '2x Xeon® Scalable de 4th geração'),
+       (44, 'Memória', 'Suporta até 4TB, RDIMM DDR5-4800MHz ECC 16 slots DIMM'),
+       (44, 'Armazenamento', ' 3x 3.5″ hot-swap NVMe/SATA'),
+       (44, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-221BT-HNR(SYS-221BT-HNR) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (45, 'Modelo', 'SYS-221BT-HNR'),
+       (45, 'Tipo', 'RACK MULTINODE TWIN 2U'),
+       (45, 'Processador', '2x  Xeon® Scalable de 4th geração'),
+       (45, 'Memória', 'Suporta até 4TB, RDIMM DDR5-4800MHz ECC 16 slots DIMM'),
+       (45, 'Armazenamento', '6x 2.5″ hot-swap NVMe'),
+       (45, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-F511E2-RT(SYS-F511E2-RT) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (46, 'Modelo', 'SYS-F511E2-RT'),
+       (46, 'Tipo', 'RACK MULTINODE TWIN 2U'),
+       (46, 'Processador', '2x  Xeon® Scalable de 4th geração'),
+       (46, 'Memória', 'Suporta até 4TB, DDR5-4800MHz 16 slots DIMM'),
+       (46, 'Armazenamento', '6x SAS/SATA/NVMe Hot Swap 2.5'),
+       (46, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-F511E2-RT(SYS-F511E2-RT) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (47, 'Modelo', 'SYS-F511E2-RT'),
+       (47, 'Tipo', 'RACK MULTINODE TWIN 2U'),
+       (47, 'Processador', '2x  Xeon® Scalable de 4th geração'),
+       (47, 'Memória', 'Suporta até 4TB, DDR5-4800MHz 16 slots DIMM'),
+       (47, 'Armazenamento', '6x SAS/SATA/NVMe Hot Swap 2.5'),
+       (47, 'Sistema Operacional', 'Windows Server 2022 Standard, Windows Server 2022 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-F511E2-RT(SYS-F511E2-RT) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (48, 'Modelo', 'SYS-F511E2-RT'),
+       (48, 'Tipo', 'Torre Storage Server'),
+       (48, 'Processador', 'Dual socket P (LGA 3647) Intel® Xeon® Scalable'),
+       (48, 'Memória', 'Suporta até 2 TB ECC 3DS LRDIMM DDR4-2666MHz; 16 DIMM'),
+       (48, 'Slots', '3 PCI-E 3.0 x16, 4 PCI-E 3.0 x8 (slot 2 & 3 occupied by controller and JBOD expansion port)'),
+       (48, 'Sistema Operacional', 'Windows Server 2019 Standard, Windows Server 2019 Datacenter');
+
+-- Inserindo um atributtes do servidor Positivo PowerEdge SYS-F511E2-RT(SYS-F511E2-RT) na tabela "product_attribute"
+INSERT INTO product_attribute (product_id, name, "value")
+VALUES (49, 'Modelo', 'SYS-F511E2-RT'),
+       (49, 'Tipo', 'Torre Storage Server'),
+       (49, 'Processador', 'Dual socket P (LGA 3647) Intel® Xeon® Scalable'),
+       (49, 'Memória', ' Suporta até 2 TB ECC 3DS LRDIMM DDR4-2666MHz; 16 DIMM'),
+       (49, 'Slots', '3 PCI-E 3.0 x16, 4 PCI-E 3.0 x8 (slot 2 & 3 occupied by controller and JBOD expansion port)'),
+       (49, 'Sistema Operacional', 'Windows Server 2019 Standard, Windows Server 2019 Datacenter');
