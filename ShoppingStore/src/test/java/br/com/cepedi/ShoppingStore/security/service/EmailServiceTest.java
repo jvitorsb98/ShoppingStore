1dd35d262f1e46c3316562864f1f831a99e33240
João Ramos <jvitorsb98@outlook.com>
2024-05-27 10:25:30 -0300

    Merge sub equipe 2

--- ShoppingStore/src/test/java/br/com/cepedi/ShoppingStore/security/service/EmailServiceTest.java
+++ ShoppingStore/src/test/java/br/com/cepedi/ShoppingStore/security/service/EmailServiceTest.java
@@ -2,30 +2,31 @@ package br.com.cepedi.ShoppingStore.security.service;
 
 import br.com.cepedi.ShoppingStore.security.model.entitys.User;
 import br.com.cepedi.ShoppingStore.security.repository.UserRepository;
+import br.com.cepedi.ShoppingStore.service.mail.MailService;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import jakarta.mail.MessagingException;
-import jakarta.mail.Session;
 import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
-import java.util.Map;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @DisplayName("EmailService Tests")
-@TestMethodOrder(MethodOrderer.Random.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EmailServiceTest {
 
     @Mock
@@ -49,30 +50,26 @@ public class EmailServiceTest {
     @InjectMocks
     private EmailService emailService;
 
-    @BeforeEach
-    @DisplayName("Set up")
-    void setUp() {
-        MockitoAnnotations.openMocks(this); // Initialize mocks
+    @Mock
+    private MailService mailService;
 
-        MimeMessage mimeMessage = Mockito.mock(MimeMessage.class);
-        when(emailSender.createMimeMessage()).thenReturn(mimeMessage);
+    @BeforeEach
+    void setUp() throws IOException {
+        MockitoAnnotations.openMocks(this); // Initialize mocks
+        when(freeMarkerConfigurer.getConfiguration()).thenReturn(configuration);
+        when(configuration.getTemplate(anyString())).thenReturn(template);
     }
 
     @Test
     @DisplayName("Test sendResetPasswordEmail with valid parameters")
+    @Order(1)
     void sendResetPasswordEmail_ValidParameters_EmailSent() throws Exception {
         // Arrange
         String name = "John Doe";
         String email = "john.doe@example.com";
         String token = "testToken";
-
-        MimeMessage mimeMessage = Mockito.mock(MimeMessage.class);
+        MimeMessage mimeMessage = mock(MimeMessage.class);
         when(emailSender.createMimeMessage()).thenReturn(mimeMessage);
-
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-
-        when(freeMarkerConfigurer.getConfiguration()).thenReturn(configuration);
-        when(configuration.getTemplate(anyString())).thenReturn(template);
 
         // Act
         emailService.sendResetPasswordEmail(name, email, token);
@@ -83,52 +80,19 @@ public class EmailServiceTest {
 
     @Test
     @DisplayName("Test sendResetPasswordEmail with template processing exception")
+    @Order(2)
     void sendResetPasswordEmail_TemplateProcessingException_ExceptionThrown() throws Exception {
         // Arrange
         String name = "John Doe";
         String email = "john.doe@example.com";
         String token = "testToken";
-
-        MimeMessage mimeMessage = Mockito.mock(MimeMessage.class);
+        MimeMessage mimeMessage = mock(MimeMessage.class);
         when(emailSender.createMimeMessage()).thenReturn(mimeMessage);
-
-        when(freeMarkerConfigurer.getConfiguration()).thenReturn(configuration);
-        when(configuration.getTemplate(anyString())).thenReturn(template);
         doThrow(new TemplateException("Failed to process email template", null)).when(template).process(any(), any());
 
         // Act & Assert
-        assertThrows(MessagingException.class, () -> {
-            emailService.sendResetPasswordEmail(name, email, token);
-        });
+        assertThrows(MessagingException.class, () -> emailService.sendResetPasswordEmail(name, email, token));
     }
 
-    @Test
-    @DisplayName("Test sendActivationEmail with valid parameters")
-    void sendActivationEmail_ValidParameters_EmailSent() throws Exception {
-        // Arrange
-        String name = "John Doe";
-        String email = "john.doe@example.com";
-        String token = "testToken";
-
-        User user = new User();
-        user.setEmail(email);
-
-        when(userRepository.findUserByEmail(email)).thenReturn(user);
-
-        MimeMessage mimeMessage = Mockito.mock(MimeMessage.class);
-        when(emailSender.createMimeMessage()).thenReturn(mimeMessage);
-
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-
-        when(tokenService.generateTokenForActivatedEmail(user)).thenReturn(token);
-        when(freeMarkerConfigurer.getConfiguration()).thenReturn(configuration);
-        when(configuration.getTemplate(anyString())).thenReturn(template);
-
-        // Act
-        emailService.sendActivationEmail(name, email);
-
-        // Assert
-        verify(emailSender, times(1)).send(any(MimeMessage.class));
-    }
 
 }
